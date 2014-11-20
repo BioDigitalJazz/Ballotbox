9f92ee355f0f54fed6e3ca5ffc9aa7286d3a84a8
tfchang <tingfchang@gmail.com>
2014-11-20 14:36:37 -0800

    Seeding data: presentations, slides, and survey options

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,6 @@ require 'faker'
 require_relative '../config/environment'
 require_relative '../config/database'
 
-# @seed_spec = PG_SPEC.dup
-# @seed_spec[:database] = 'ballotbox'
-# ActiveRecord::Base.establish_connection(@seed_spec)
-
 FactoryGirl.define do
   factory :presentation do
     title { Faker::Commerce.product_name }
@@ -19,13 +15,14 @@ end
   pre = FactoryGirl.create(:presentation)
   1.upto(5) do |si|
     s_type = si.odd? ? 'regular' : 'survey'
-    s_content = "<div>#{Faker::Hacker.say_something_smart}</div>"
+    # s_content = "<div>#{Faker::Hacker.say_something_smart}</div>"
+    s_content = "<div>#{Faker::Lorem.paragraph}</div>"
     sli = pre.slides.create(slide_number: si, slide_type: s_type, content: s_content)
 
     if s_type == 'survey'
-      'a'..'d'.each do |label|
-        option_text = Faker::Hacker.noun
-        sli.survey_options.create(option_label: label, text: #{option_text} )
+      ('a'..'d').each do |label|
+        option_text = Faker::Company.name
+        sli.survey_options.create(option_label: label, text: option_text)
       end
     end
   end
