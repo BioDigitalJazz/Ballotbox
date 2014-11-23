5106dbad85047a42f42a1ccf332f775ff90f61c0
fjollnir <fjollnir@gmail.com>
2014-11-22 17:15:56 -0800

    created views and adjusted actions to render next

--- app/actions.rb
+++ app/actions.rb
@@ -6,13 +6,11 @@ helpers do
   end
 
   def get_session
-    Session.where[access_code: session[:presentation_access_code]].first
+    Session.where(:access_code == session[:presentation_access_code]).first
   end
 
   def advance_slide
-    if session[:slide_number] >= get_presentation.slides.count
-      redirect '/present/:presentation_access_code'
-    else
+    if !(session[:slide_number] >= get_presentation.slides.count)
       session[:slide_number] += 1
     end
   end
@@ -27,23 +25,23 @@ get '/' do
 end
 
 get '/go_to/present/:presentation_access_code' do
-  session[presentation_access_code: params[:presentation_access_code]]
+  session[:presentation_access_code] = params[:presentation_access_code]
   session[:slide_number] = 1
-  redirect "/present/:#{session[:presentation_acces_code]}/:#{session[:slide_number]}"
+  redirect "/present/#{session[:presentation_access_code]}/#{session[:slide_number]}"
+end
+
+get '/present/:presentation_access_code/next' do
+  advance_slide
+  redirect "/present/#{session[:presentation_access_code]}/#{session[:slide_number]}"
 end
 
 get '/present/:presentation_access_code/:slide_number' do
   erb :'/present/presentation'
 end
 
-get '/present/:presentation_access_code/next_slide' do
-  advance_slide
-  redirect "/present/:#{session[:presentation_acces_code]}/:#{session[:slide_number]}"
-end
-
 get '/present/:presentation_access_code/previous_slide' do
   back_slide
-  redirect "/present/:#{session[:presentation_acces_code]}/:#{session[:slide_number]}"
+  redirect "/present/:#{session[:presentation_access_code]}/:#{session[:slide_number]}"
 end
 
 
