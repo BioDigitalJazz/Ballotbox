775f864ecafbd17b1e16bdda01b356fdda1a9bd4
Dave Hyatt <dhyatt182@gmail.com>
2014-11-25 13:32:47 -0800

    images folder changed back to img

--- public/javascript/test_pie.js
+++ public/javascript/test_pie.js
@@ -2,7 +2,7 @@
 
 var ctx = document.getElementById("myChart").getContext("2d");
 
-var chart_colors = ["red", "green", "blue", "yellow", "maroon", "pink", "purple", "orange"];
+var chart_colors = ["rgba(61, 201, 179, 1)", "#8C8080", "#242830", "yellow", "maroon", "pink", "purple", "orange"];
 var data = [];
 var index;
 
@@ -15,7 +15,7 @@ for (index = 0; index < gon.survey_options.length; index++) {
     // alert(gon.survey_feedbacks[index][:votes)
 };
 
-var legendStr = "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"color:<%=segments[i].fillColor%>;font-size:20px;font-weight:bold;\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>"
+var legendStr = "<ul style=\"list-style-type: none;\" class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"color:<%=segments[i].fillColor%>;font-size:20px;font-weight:bold;\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>"
 
 var myNewChart = new Chart(ctx).Pie(data, { legendTemplate : legendStr });
 var legend = myNewChart.generateLegend();
